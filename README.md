93d03b84749bdbc6c826dfb5d83a55bd71814683
antonellil <lpa22@cornell.edu>
2013-12-03 18:55:23 -0500

    updated readme and stats_spider works for team and game dictionaries

--- README.md
+++ README.md
@@ -9,16 +9,39 @@ nfl-spread
 
 ### Player Dictionary Layout
 The player dictionary can be found in players.json and is indexed by player, then by season, then by week. Every season and week that a player was active is present in the dictionary. The value for the week is a size 11 array with values as follows:
-1) pass_completions
-2) pass_attempts
-3) pass_yards
-4) pass_TDs
-5) pass_INTs
-6) rush_attempts
-7) rush_yards
-8) rush_TDs
-9) receiving_rec
-10) receiving_yards
-11) receiving_TDs
+*1) pass_completions
+*2) pass_attempts
+*3) pass_yards
+*4) pass_TDs
+*5) pass_INTs thrown
+*6) rush_attempts
+*7) rush_yards
+*8) rush_TDs
+*9) receiving_rec
+*10) receiving_yards
+*11) receiving_TDs
 
-Given a roster, number of games, week, year, and home (bool), the roster_stats function in instance_generator.py sums up the stats from the previous given number of games before the given week and season. Still working on getting home to work.
+Given a roster, number of games, week, year, and home (bool), the roster_stats function in instance_generator.py sums up the stats from the previous given number of games before the given week and season.
+
+### Team Dictionary Layout
+The team dictionary can be found in teams.json and is indexed by team, then by season, then by week. Every season and week that a team was active is present in the dictionary. The value for the week is a size 18 array with values as follows:
+*1) first downs allowed
+*2) rush_attempts against
+*3) rush_yards allowed
+*4) rush_TDs allowed
+*5) pass_completions allowed
+*6) pass_attempts against
+*7) pass_yards allowed
+*8) pass_TDs allowed
+*9) pass_INTs
+*10) sacks
+*11) sacks_yards
+*12) net passing yards (useless)
+*13) total_yards allowed
+*14) fumbles of the other team
+*15) fumbles_recovered
+*16) total_turnovers forced
+*17) penalties of other team
+*18) penalty yards of other team
+
+Given a roster, number of games, week, year, and home (bool), the team_stats function in instance_generator.py sums up the stats from the previous given number of games before the given week and season.
--- teamstats/teamstats/spiders/stats_spider.py
+++ teamstats/teamstats/spiders/stats_spider.py
@@ -18,7 +18,8 @@ from math import ceil
 # in the NFL between the 'min-year' and 'max-year' values
 #
 
-team_dictionary= {}
+team_dictionary = {}
+game_dictionary = {}
 
 class TeamStatsSpider(CrawlSpider):
 
@@ -28,7 +29,7 @@ class TeamStatsSpider(CrawlSpider):
 
   # The range of years (these can later be added as parameters)
   min_year = 1998
-  max_year = 2013
+  max_year = 2014
 
   # Develop the start urls based on the years
   start_urls = []
@@ -72,259 +73,91 @@ class TeamStatsSpider(CrawlSpider):
 
         # Gets the final scores
         score_table = sel.xpath('//table[@id="linescore"]/tr/td/text()').extract()
-        team_one_score = int(score_table[5])
-        team_two_score = int(score_table[11])
-        margin = team_one_score - team_two_score
+        away_score = int(score_table[5])
+        home_score = int(score_table[11])
+        # home - away
+        margin = home_score - away_score
 
         # Gets each team!
         thead = sel.xpath('//table[@id="team_stats"]/tr/th/node()').extract()
-        team_one = thead[0]
-        team_two = thead[1]
+        away_team = thead[0]
+        home_team = thead[1]
 
-        # Add the team to the dictionary if need be
-        if team_one not in team_dictionary:
-          team_dictionary[team_one] = {}
-        if team_two not in team_dictionary:
-          team_dictionary[team_two] = {}
+        # Add the team to the team_dictionary if need be
+        if away_team not in team_dictionary:
+          team_dictionary[away_team] = {}
+        if home_team not in team_dictionary:
+          team_dictionary[home_team] = {}
 
-        # Add the year to the dictionary if need be
-        if year not in team_dictionary[team_one]:
-          team_dictionary[team_one][year] = {}
-        if year not in team_dictionary[team_two]:
-          team_dictionary[team_two][year] = {}
+        # Add the year to the team_dictionary if need be
+        if year not in team_dictionary[away_team]:
+          team_dictionary[away_team][year] = {}
+        if year not in team_dictionary[home_team]:
+          team_dictionary[home_team][year] = {}
 
-        defensive_stats_one = {}
-        defensive_stats_two = {}
+        # Add the year to the game_dictionary if need be
+        if year not in game_dictionary:
+          game_dictionary[year] = {}
 
-        ptable = sel.xpath('//table[@id="skill_stats"]/tbody/tr/node()').extract()
+        # Initialize the week in the game_dictionary if need be
+        if week not in game_dictionary[year]:
+          game_dictionary[year][week] = []
 
-        cn = 0
-        team_one_roster = []
-        team_two_roster = []
+        ptable = sel.xpath('//table[@id="skill_stats"]/tbody/tr')
 
-        # grabs the individual players from the Passing, Rushing, & Receiving table
-        for skill in ptable:
-          if cn % 33 == 10:
-            if ("td" in skill):
-              st = skill[66:]
-              st = st.replace("</a></td>", "")
-              team_two_roster.append(st)
-          if cn % 33 == 1:
-            if ("align=\"left\"" in skill):
-              st = skill[66:]
-              st = st.replace("</a></td>", "")
-              team_one_roster.append(st)
-          cn = cn + 1
+        away_roster = []
+        home_roster = []
 
-      defensive_stats_one['roster'] = team_one_roster
-      defensive_stats_two['roster'] = team_two_roster
-      defensive_stats_one['margin'] = margin
-      defensive_stats_two['margin'] = -margin
+        # grabs the individual players to create the roster from the Passing, Rushing, & Receiving table
+        for player in ptable:
+          player_team = player.xpath('td[2]/text()').extract()
+          if player_team != [] and player_team[0] == home_team:
+            home_roster.append(player.xpath('td[1]/a/text()').extract()[0])
+          elif player_team != [] and player_team[0] == away_team:
+            away_roster.append(player.xpath('td[1]/a/text()').extract()[0])
 
-      # Get the team stats data and save the defensive stuff
-      table = sel.xpath('//table[@id="team_stats"]/tr/td/node()').extract()
-      counter = 0
-      for row in table:
-        row_num = ceil(counter / 3)
-        col_num = counter % 3
-        if col_num == 1:
-          if row_num == 0:
-            defensive_stats_two['first_downs'] = row
-          elif row_num == 1:
-            split = row.split("-")
-            defensive_stats_two['rush_attempts'] = split[0]
-            defensive_stats_two['rush_yards'] = split[1]
-            defensive_stats_two['rush_TDs'] = split[2]
-          elif row_num == 2:
-            split = row.split("-")
-            defensive_stats_two['completions'] = split[0]
-            defensive_stats_two['attempts'] = split[1]
-            defensive_stats_two['passing_yards'] = split[2]
-            defensive_stats_two['passing_TDs'] = split[3]
-            defensive_stats_two['passing_INTs'] = split[4]
-          elif row_num == 3:
-            split = row.split("-")
-            defensive_stats_two['sacks'] = split[0]
-            defensive_stats_two['sack_yards'] = split[1]
-          elif row_num == 4:
-            defensive_stats_two['net_pass_yards'] = row
-          elif row_num == 5:
-            defensive_stats_two['net_yards'] = row
-          elif row_num == 6:
-            split = row.split("-")
-            defensive_stats_two['fumbles'] = split[0]
-            defensive_stats_two['fumbles_lost'] = split[1]
-          elif row_num == 7:
-            defensive_stats_two['turnovers'] = row
-          elif row_num == 8:
-            split = row.split("-")
-            defensive_stats_two['penalties'] = split[0]
-            defensive_stats_two['penalty_yards'] = split[1]
-          else:
-            pass
-        elif col_num == 2:
-          if row_num == 0:
-            defensive_stats_one['first_downs'] = row
-          elif row_num == 1:
-            split = row.split("-")
-            defensive_stats_one['rush_attempts'] = split[0]
-            defensive_stats_one['rush_yards'] = split[1]
-            defensive_stats_one['rush_TDs'] = split[2]
-          elif row_num == 2:
-            split = row.split("-")
-            defensive_stats_one['completions'] = split[0]
-            defensive_stats_one['attempts'] = split[1]
-            defensive_stats_one['passing_yards'] = split[2]
-            defensive_stats_one['passing_TDs'] = split[3]
-            defensive_stats_one['passing_INTs'] = split[4]
-          elif row_num == 3:
-            split = row.split("-")
-            defensive_stats_one['sacks'] = split[0]
-            defensive_stats_one['sack_yards'] = split[1]
-          elif row_num == 4:
-            defensive_stats_one['net_pass_yards'] = row
-          elif row_num == 5:
-            defensive_stats_one['net_yards'] = row
-          elif row_num == 6:
-            split = row.split("-")
-            defensive_stats_one['fumbles'] = split[0]
-            defensive_stats_one['fumbles_lost'] = split[1]
-          elif row_num == 7:
-            defensive_stats_one['turnovers'] = row
-          elif row_num == 8:
-            split = row.split("-")
-            defensive_stats_one['penalties'] = split[0]
-            defensive_stats_one['penalty_yards'] = split[1]
-          else:
-            pass
-        else:
-          pass
-        counter = counter + 1
+        # Construct the game instance in the game_dictionary
+        game_dictionary[year][week].append({
+          'home': home_team,
+          'away': away_team,
+          'home_roster': home_roster,
+          'away_roster': away_roster,
+          'margin': margin
+        })
 
+        # Get the team stats data 
+        team_table = sel.xpath('//table[@id="team_stats"]/tr')
 
-      team_dictionary[team_one][year][week] = defensive_stats_one
-      team_dictionary[team_two][year][week] = defensive_stats_two
+        # Store the defensive stats for home and away
+        defensive_stats_home, defensive_stats_away = self.generate_defensive_stats(team_table)
 
+        # Set the defensive stats in the team_dictionary
+        team_dictionary[home_team][year][week] = defensive_stats_home
+        team_dictionary[away_team][year][week] = defensive_stats_away
 
+  def generate_defensive_stats(self,team_table):
+    defensive_stats_home = []
+    defensive_stats_away = []
 
-      '''# THIS GRABS THE TABLE SPECIFICALLY WITH ID 'TEAM_STATS'. MODIFY THIS TO
-      # GET ANY OTHER TABLE
-      thead = sel.xpath('//table[@id="team_stats"]/tr/th/node()').extract()
-      table = sel.xpath('//table[@id="team_stats"]/tr/td/node()').extract()
+    for stat_line in team_table[1:]:
+      raw_home_stats = stat_line.xpath('td[3]/text()').extract()[0].split('-')
+      raw_away_stats = stat_line.xpath('td[2]/text()').extract()[0].split('-')
+      
+      # Do the opposite to store defensive stats
+      for stat in raw_away_stats:
+        defensive_stats_home.append(int(stat))
+      for stat in raw_home_stats:
+        defensive_stats_away.append(int(stat))
 
-      counter = 0
-      # Create the game item
-      team_item_one= GameItem()
-      team_item_one['team_one'] = team_one
-      team_item_one['team_two'] = team_two
-
-      cn = 0
-      team_one_roster = []
-      team_two_roster = []
-
-      # grabs the individual players from the Passing, Rushing, & Receiving table
-      for skill in ptable:
-        if cn % 33 == 10:
-          if ("td" in skill):
-            st = skill[66:]
-            st = st.replace("</a></td>", "")
-            team_two_roster.append(st)
-        if cn % 33 == 1:
-          if ("align=\"left\"" in skill):
-            st = skill[66:]
-            st = st.replace("</a></td>", "")
-            team_one_roster.append(st)
-        cn = cn + 1
-
-      team_item_one['roster_one'] = team_one_roster
-      team_item_one['roster_two'] = team_two_roster
-
-      # This prints all the table data
-      for row in table:
-        row_num = ceil(counter / 3)
-        col_num = counter % 3
-        if col_num == 1:
-          if row_num == 0:
-            team_item_one['first_downs_one'] = row
-          elif row_num == 1:
-            split = row.split("-")
-            team_item_one['rush_attempts_one'] = split[0]
-            team_item_one['rush_yards_one'] = split[1]
-            team_item_one['rush_TDs_one'] = split[2]
-          elif row_num == 2:
-            split = row.split("-")
-            team_item_one['completions_one'] = split[0]
-            team_item_one['attempts_one'] = split[1]
-            team_item_one['passing_yards_one'] = split[2]
-            team_item_one['passing_TDs_one'] = split[3]
-            team_item_one['passing_INTs_one'] = split[4]
-          elif row_num == 3:
-            split = row.split("-")
-            team_item_one['sacks_one'] = split[0]
-            team_item_one['sack_yards_one'] = split[1]
-          elif row_num == 4:
-            team_item_one['net_pass_yards_one'] = row
-          elif row_num == 5:
-            team_item_one['net_yards_one'] = row
-          elif row_num == 6:
-            split = row.split("-")
-            team_item_one['fumbles_one'] = split[0]
-            team_item_one['fumbles_lost_one'] = split[1]
-          elif row_num == 7:
-            team_item_one['turnovers_one'] = row
-          elif row_num == 8:
-            split = row.split("-")
-            team_item_one['penalties_one'] = split[0]
-            team_item_one['penalty_yards_one'] = split[1]
-          else:
-            pass
-        elif col_num == 2:
-          if row_num == 0:
-            team_item_one['first_downs_two'] = row
-          elif row_num == 1:
-            split = row.split("-")
-            team_item_one['rush_attempts_two'] = split[0]
-            team_item_one['rush_yards_two'] = split[1]
-            team_item_one['rush_TDs_two'] = split[2]
-          elif row_num == 2:
-            split = row.split("-")
-            team_item_one['completions_two'] = split[0]
-            team_item_one['attempts_two'] = split[1]
-            team_item_one['passing_yards_two'] = split[2]
-            team_item_one['passing_TDs_two'] = split[3]
-            team_item_one['passing_INTs_two'] = split[4]
-          elif row_num == 3:
-            split = row.split("-")
-            team_item_one['sacks_two'] = split[0]
-            team_item_one['sack_yards_two'] = split[1]
-          elif row_num == 4:
-            team_item_one['net_pass_yards_two'] = row
-          elif row_num == 5:
-            team_item_one['net_yards_two'] = row
-          elif row_num == 6:
-            split = row.split("-")
-            team_item_one['fumbles_two'] = split[0]
-            team_item_one['fumbles_lost_two'] = split[1]
-          elif row_num == 7:
-            team_item_one['turnovers_two'] = row
-          elif row_num == 8:
-            split = row.split("-")
-            team_item_one['penalties_two'] = split[0]
-            team_item_one['penalty_yards_two'] = split[1]
-          else:
-            pass
-        else:
-          pass
-        counter = counter + 1'''
-        #self.log(row)'''
-
-
-      #return team_item_one
+    return defensive_stats_home, defensive_stats_away
 
   def spider_closed(self):
     teams = open('teams.json', 'w')
     teams.write(json.dumps(team_dictionary, sort_keys=True,separators=(',',':')))
     teams.close()
+    games = open('games.json','w')
+    games.write(json.dumps(game_dictionary,sort_keys=True,separators=(',',':')))
+    games.close()
 
 
 
