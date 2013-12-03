499e95d28783d04a146dd8a7f680b864f33b4034
ardot <tel36@cornell.edu>
2013-12-03 16:52:54 -0500

    Update stats_spider.py

--- teamstats/teamstats/spiders/stats_spider.py
+++ teamstats/teamstats/spiders/stats_spider.py
@@ -1,8 +1,11 @@
+import json
 #from scrapy.spider import CrawlSpider
 from scrapy.selector import Selector
 from scrapy.http.request import Request
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.xlib.pydispatch import dispatcher
+from scrapy import signals
 
 from teamstats.items import GameItem
 from math import ceil
@@ -15,6 +18,8 @@ from math import ceil
 # in the NFL between the 'min-year' and 'max-year' values
 #
 
+team_dictionary= {}
+
 class TeamStatsSpider(CrawlSpider):
 
   # Start-up required params
@@ -22,8 +27,8 @@ class TeamStatsSpider(CrawlSpider):
   allowed_domains = ['pro-football-reference.com']
 
   # The range of years (these can later be added as parameters)
-  min_year = 1992
-  max_year = 1993
+  min_year = 1998
+  max_year = 2013
 
   # Develop the start urls based on the years
   start_urls = []
@@ -32,6 +37,8 @@ class TeamStatsSpider(CrawlSpider):
       'http://www.pro-football-reference.com/years/' + str(i) + '/games.htm'
     )
 
+  def __init__(self):
+    dispatcher.connect(self.spider_closed, signals.spider_closed)
 
   # Scrapy automatically calls this method, sequentially, for every url in the
   # 'start_urls' array
@@ -56,20 +63,181 @@ class TeamStatsSpider(CrawlSpider):
   def parse_page(self, response):
       sel = Selector(response)
 
+      game_time = sel.xpath('//table[1]/tr/td[1]/node()').extract()
+
+      if str(game_time[0]).split(' ')[0].lower() == 'week':
+        # Gets the year and week
+        week = int(game_time[0].split(' ')[1])
+        year = int(game_time[2])
+
+        # Gets the final scores
+        score_table = sel.xpath('//table[@id="linescore"]/tr/td/text()').extract()
+        team_one_score = int(score_table[5])
+        team_two_score = int(score_table[11])
+        margin = team_one_score - team_two_score
+
+        # Gets each team!
+        thead = sel.xpath('//table[@id="team_stats"]/tr/th/node()').extract()
+        team_one = thead[0]
+        team_two = thead[1]
+
+        # Add the team to the dictionary if need be
+        if team_one not in team_dictionary:
+          team_dictionary[team_one] = {}
+        if team_two not in team_dictionary:
+          team_dictionary[team_two] = {}
+
+        # Add the year to the dictionary if need be
+        if year not in team_dictionary[team_one]:
+          team_dictionary[team_one][year] = {}
+        if year not in team_dictionary[team_two]:
+          team_dictionary[team_two][year] = {}
+
+        defensive_stats_one = {}
+        defensive_stats_two = {}
+
+        ptable = sel.xpath('//table[@id="skill_stats"]/tbody/tr/node()').extract()
+
+        cn = 0
+        team_one_roster = []
+        team_two_roster = []
+
+        # grabs the individual players from the Passing, Rushing, & Receiving table
+        for skill in ptable:
+          if cn % 33 == 10:
+            if ("td" in skill):
+              st = skill[66:]
+              st = st.replace("</a></td>", "")
+              team_two_roster.append(st)
+          if cn % 33 == 1:
+            if ("align=\"left\"" in skill):
+              st = skill[66:]
+              st = st.replace("</a></td>", "")
+              team_one_roster.append(st)
+          cn = cn + 1
+
+      defensive_stats_one['roster'] = team_one_roster
+      defensive_stats_two['roster'] = team_two_roster
+      defensive_stats_one['margin'] = margin
+      defensive_stats_two['margin'] = -margin
+
+      # Get the team stats data and save the defensive stuff
+      table = sel.xpath('//table[@id="team_stats"]/tr/td/node()').extract()
+      counter = 0
+      for row in table:
+        row_num = ceil(counter / 3)
+        col_num = counter % 3
+        if col_num == 1:
+          if row_num == 0:
+            defensive_stats_two['first_downs'] = row
+          elif row_num == 1:
+            split = row.split("-")
+            defensive_stats_two['rush_attempts'] = split[0]
+            defensive_stats_two['rush_yards'] = split[1]
+            defensive_stats_two['rush_TDs'] = split[2]
+          elif row_num == 2:
+            split = row.split("-")
+            defensive_stats_two['completions'] = split[0]
+            defensive_stats_two['attempts'] = split[1]
+            defensive_stats_two['passing_yards'] = split[2]
+            defensive_stats_two['passing_TDs'] = split[3]
+            defensive_stats_two['passing_INTs'] = split[4]
+          elif row_num == 3:
+            split = row.split("-")
+            defensive_stats_two['sacks'] = split[0]
+            defensive_stats_two['sack_yards'] = split[1]
+          elif row_num == 4:
+            defensive_stats_two['net_pass_yards'] = row
+          elif row_num == 5:
+            defensive_stats_two['net_yards'] = row
+          elif row_num == 6:
+            split = row.split("-")
+            defensive_stats_two['fumbles'] = split[0]
+            defensive_stats_two['fumbles_lost'] = split[1]
+          elif row_num == 7:
+            defensive_stats_two['turnovers'] = row
+          elif row_num == 8:
+            split = row.split("-")
+            defensive_stats_two['penalties'] = split[0]
+            defensive_stats_two['penalty_yards'] = split[1]
+          else:
+            pass
+        elif col_num == 2:
+          if row_num == 0:
+            defensive_stats_one['first_downs'] = row
+          elif row_num == 1:
+            split = row.split("-")
+            defensive_stats_one['rush_attempts'] = split[0]
+            defensive_stats_one['rush_yards'] = split[1]
+            defensive_stats_one['rush_TDs'] = split[2]
+          elif row_num == 2:
+            split = row.split("-")
+            defensive_stats_one['completions'] = split[0]
+            defensive_stats_one['attempts'] = split[1]
+            defensive_stats_one['passing_yards'] = split[2]
+            defensive_stats_one['passing_TDs'] = split[3]
+            defensive_stats_one['passing_INTs'] = split[4]
+          elif row_num == 3:
+            split = row.split("-")
+            defensive_stats_one['sacks'] = split[0]
+            defensive_stats_one['sack_yards'] = split[1]
+          elif row_num == 4:
+            defensive_stats_one['net_pass_yards'] = row
+          elif row_num == 5:
+            defensive_stats_one['net_yards'] = row
+          elif row_num == 6:
+            split = row.split("-")
+            defensive_stats_one['fumbles'] = split[0]
+            defensive_stats_one['fumbles_lost'] = split[1]
+          elif row_num == 7:
+            defensive_stats_one['turnovers'] = row
+          elif row_num == 8:
+            split = row.split("-")
+            defensive_stats_one['penalties'] = split[0]
+            defensive_stats_one['penalty_yards'] = split[1]
+          else:
+            pass
+        else:
+          pass
+        counter = counter + 1
 
 
-      # THIS GRABS THE TABLE SPECIFICALLY WITH ID 'TEAM_STATS'. MODIFY THIS TO
+      team_dictionary[team_one][year][week] = defensive_stats_one
+      team_dictionary[team_two][year][week] = defensive_stats_two
+
+
+
+      '''# THIS GRABS THE TABLE SPECIFICALLY WITH ID 'TEAM_STATS'. MODIFY THIS TO
       # GET ANY OTHER TABLE
       thead = sel.xpath('//table[@id="team_stats"]/tr/th/node()').extract()
       table = sel.xpath('//table[@id="team_stats"]/tr/td/node()').extract()
-      # Team names are stored in the table head
-      team_one = thead[0]
-      team_two = thead[1]
+
       counter = 0
       # Create the game item
       team_item_one= GameItem()
       team_item_one['team_one'] = team_one
       team_item_one['team_two'] = team_two
+
+      cn = 0
+      team_one_roster = []
+      team_two_roster = []
+
+      # grabs the individual players from the Passing, Rushing, & Receiving table
+      for skill in ptable:
+        if cn % 33 == 10:
+          if ("td" in skill):
+            st = skill[66:]
+            st = st.replace("</a></td>", "")
+            team_two_roster.append(st)
+        if cn % 33 == 1:
+          if ("align=\"left\"" in skill):
+            st = skill[66:]
+            st = st.replace("</a></td>", "")
+            team_one_roster.append(st)
+        cn = cn + 1
+
+      team_item_one['roster_one'] = team_one_roster
+      team_item_one['roster_two'] = team_two_roster
 
       # This prints all the table data
       for row in table:
@@ -147,9 +315,16 @@ class TeamStatsSpider(CrawlSpider):
             pass
         else:
           pass
-        counter = counter + 1
-        #self.log(row)
+        counter = counter + 1'''
+        #self.log(row)'''
 
-      return team_item_one
+
+      #return team_item_one
+
+  def spider_closed(self):
+    teams = open('teams.json', 'w')
+    teams.write(json.dumps(team_dictionary, sort_keys=True,separators=(',',':')))
+    teams.close()
+
 
 
