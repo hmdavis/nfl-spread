16b59e979b90fc6e2609663db429cc5282aa79de
Harry <harrisonmaxwelldavis@gmail.com>
2013-12-04 20:49:28 -0500

    modified algos and ran on all datasets

--- model.py
+++ model.py
@@ -1,14 +1,15 @@
 '''
 Test our data with various machine learned models
 ''' 
-import sys, math
+import sys, math, argparse
 import numpy as np 
+import pylab as pl 
 from sklearn import cross_validation, neighbors
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.grid_search import GridSearchCV
 from sklearn.datasets import load_files 
-import pylab as pl 
-import argparse
+
+
 
 ''' 
 Prepare the dataset to be used in algorithms 
@@ -101,24 +102,46 @@ Params:
 '''
 def knn(X,y):
 	# split the data into train, test set 
-	X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y,test_size=0.3, random_state=0)
-
+	# X, X_val, y, y_val = cross_validation.train_test_split(X,y,test_size=0.2, random_state=0)
+	k_fold = cross_validation.KFold(n=len(y), n_folds=5, indices=True, shuffle=True)
 	n_neighbors = [1,2,5,10,50,100]
+
 	for k in n_neighbors: 
-		knn = neighbors.KNeighborsRegressor(k)
-		knn.fit(X_train, y_train)
+		for train, test in k_fold:
 
-		print "############## TRAINING ERROR, k=", k, "##############"
-		pred = knn.predict(X_train)
-		print "Spread:\t", avg_spread_difference(pred, y_train, True)
-		print "Winner:\t", percent_same_winner(pred, y_train, True) 
-		print "Score:\t", knn.score(X_train, y_train)
+			spreads_train = []
+			spreads_test = []
+			acc_train = [] 
+			acc_test = [] 
+			score_train = [] 
+			score_test = [] 
+			
+			X_train , y_train = (X[train], y[train])
+			X_test, y_test = (X[test], y[test])
+			knn = neighbors.KNeighborsRegressor(k)
+			knn.fit(X_train, y_train)
 
-		print "############## TESTING ERROR, k=", k, "##############"
-		pred = knn.predict(X_test)
-		print "Spread:\t", avg_spread_difference(pred, y_test, True)
-		print "Winner:\t", percent_same_winner(pred, y_test, True) 
-		print "Score:\t", knn.score(X_test, y_test)
+			# calculate training errors 
+			pred = knn.predict(X_train)
+			spreads_train.append(avg_spread_difference(pred, y_train, True))
+			acc_train.append(percent_same_winner(pred, y_train, True)) 
+			score_train.append(knn.score(X_train, y_train))
+
+			# calculate test errors 
+			pred = knn.predict(X_test)
+			spreads_test.append(avg_spread_difference(pred, y_test, True))
+			acc_test.append(percent_same_winner(pred, y_test, True)) 
+			score_test.append(knn.score(X_test, y_test))
+
+		print "\n>> TRAIN, k=", k 
+		print "Avg. Spread:\t", sum(spreads_train) / float(len(spreads_train))
+		print "Avg. Winner:\t", sum(acc_train) / float(len(acc_train))
+		print "Avg. R^2 Score:\t", sum(score_train) / float(len(score_train))
+
+		print "\n>> TEST, k=", k 
+		print "Avg. Spread:\t", sum(spreads_test) / float(len(spreads_test))
+		print "Avg. Winner:\t", sum(acc_test) / float(len(acc_test))
+		print "Avg. R^2 Score:\t", sum(score_test) / float(len(score_test))
 
 
 ''' 
@@ -129,33 +152,52 @@ Params:
 '''
 def dt(X,y): 
 	# split data 
-	X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y,test_size=0.3, random_state=0)
-
+	# X, X_val, y, y_val = cross_validation.train_test_split(X,y,test_size=0.2, random_state=0)
+	k_fold = cross_validation.KFold(n=len(y), n_folds=5, indices=True, shuffle=True)
 	max_depth = [1,2, 3, 5, 10, 50, 80, 100]
 	
 	for d in max_depth:
-	# straight up, no funny business 
-		dt = DecisionTreeRegressor(max_depth=d)
-		dt.fit(X_train, y_train)
+		for train, test in k_fold:
+
+			spreads_train = []
+			spreads_test = []
+			acc_train = [] 
+			acc_test = [] 
+			score_train = [] 
+			score_test = [] 
+			
+			X_train , y_train = (X[train], y[train])
+			X_test, y_test = (X[test], y[test])
+			dt = DecisionTreeRegressor(max_depth=d)
+			dt.fit(X_train, y_train)
 		
-		print "############## TRAINING ERROR, d=", d, "##############"
-		pred = dt.predict(X_train)[:]
-		print "Spread:\t", avg_spread_difference(pred, y_train, False)
-		print "Winner:\t", percent_same_winner(pred, y_train, False) 
-		print "Score:\t", dt.score(X_train, y_train)
+			# calculate training error 
+			pred = dt.predict(X_train)[:]
+			spreads_train.append(avg_spread_difference(pred, y_train, False))
+			acc_train.append(percent_same_winner(pred, y_train, False)) 
+			score_train.append(dt.score(X_train, y_train))
 
-		print "############## TESTING ERROR, d=", d, "##############"
-		pred = dt.predict(X_test)[:]
-		print "Spread:\t", avg_spread_difference(pred, y_test, False)
-		print "Winner:\t", percent_same_winner(pred, y_test, False) 
-		print "Score:\t", dt.score(X_test, y_test)
+			pred = dt.predict(X_test)[:]
+			spreads_test.append(avg_spread_difference(pred, y_test, False))
+			acc_test.append(percent_same_winner(pred, y_test, False)) 
+			score_test.append(dt.score(X_test, y_test))
 
+		print "\n>> TRAIN, d=", d 
+		print "Avg. Spread:\t", sum(spreads_train) / float(len(spreads_train))
+		print "Avg. Winner:\t", sum(acc_train) / float(len(acc_train))
+		print "Avg. R^2 Score:\t", sum(score_train) / float(len(score_train))
+
+		print "\n>> TEST, d=", d 
+		print "Avg. Spread:\t", sum(spreads_test) / float(len(spreads_test))
+		print "Avg. Winner:\t", sum(acc_test) / float(len(acc_test))
+		print "Avg. R^2 Score:\t", sum(score_test) / float(len(score_test))
+		
+
+###############################################################################
 argparser = argparse.ArgumentParser()
 argparser.add_argument('filename', type=file) 
 argparser.add_argument('--knn', action='store_true', default=False)
 argparser.add_argument('--dt', action='store_true', default=False)
-
-
 args = argparser.parse_args()
 
 X, y = load_data(args.filename)
